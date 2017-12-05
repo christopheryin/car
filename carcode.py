0ac084d89a66ed5e7c401bbeaee7dbe7b41aa4fd
christopheryin <chhyin136@gmail.com>
2017-12-04 18:57:26 -0800

    white edge detection w/ yellow center lane

--- carcode.py
+++ carcode.py
@@ -2,8 +2,6 @@ import numpy as np
 import cv2
 
 
-
-
 def colorProc(img_path):
 
     img = cv2.imread(img_path, cv2.IMREAD_COLOR)
@@ -15,43 +13,47 @@ def colorProc(img_path):
     upper_yellow = np.array([110, 255, 255], dtype='uint8')
     mask_yellow = cv2.inRange(hsv, lower_yellow, upper_yellow)
 
-    neg_yellow_mask = cv2.bitwise_not(mask_yellow)
-    neg_yellow = cv2.bitwise_and(gray,neg_yellow_mask)
-
     # limits of white mask
-    lower_white = np.array([0, 0, 0], dtype='uint8')
-    upper_white = np.array([255, 255, 255], dtype='uint8')
+    lower_white = np.array([0, 0, 170], dtype='uint8')
+    upper_white = np.array([255, 85, 255], dtype='uint8')
     mask_white = cv2.inRange(hsv, lower_white, upper_white)
 
-    y = np.zeros(img.shape)
-    w = np.zeros(img.shape)
+    y = cv2.bitwise_and(img,img,mask=mask_yellow)
+    w = cv2.bitwise_and(img,img,mask=mask_white)
 
-    for i in range(3):
-        imslice = img[:,:,i]
-        y[:,:,i] = cv2.bitwise_and(imslice,mask_yellow)
-        w[:,:,i] = cv2.bitwise_and(imslice,mask_white)
+    y = y*255
+#    y[:,:,0] = mask_yellow*255
+#    y[:,:,1] = mask_yellow*255
+#    y[:,:,2] = mask_yellow*0
+
+    # reduce noise
+    gauss_gray = cv2.GaussianBlur(w, (3, 3), 0)
+
+    # canny edge
+    low_threshold = 50
+    high_threshold = 160
+    w = cv2.Canny(gauss_gray, low_threshold, high_threshold)
+    w = cv2.cvtColor(w,cv2.COLOR_GRAY2BGR)
 
     yw = y + w
 
     img = yw
-
     vlim1 = 30
     vlim2 = 105
     hlim1 = 10
     hlim2 = 150
 
-    h,w,d = img.shape
-    mask = np.ones([h,w,d])
-    mask[0:vlim1, :,:] = np.zeros([vlim1, w,d])
-    mask[vlim2:h, :,:] = np.zeros([(h - vlim2), w,d])
-    mask[:, 0:hlim1,:] = np.zeros([h, hlim1,d])
-    mask[:, hlim2:w,:] = np.zeros([h, w - hlim2,d])
+    h, w, d = img.shape
+    mask = np.ones([h, w, d])
+    mask[0:vlim1, :, :] = np.zeros([vlim1, w, d])
+    mask[vlim2:h, :, :] = np.zeros([(h - vlim2), w, d])
+    mask[:, 0:hlim1, :] = np.zeros([h, hlim1, d])
+    mask[:, hlim2:w, :] = np.zeros([h, w - hlim2, d])
 
     mask = np.zeros(img.shape, dtype="uint8")
-    img = img[vlim1:vlim2, hlim1:hlim2,:]
-    mask[vlim1:vlim2, hlim1:hlim2,:] = cv2.bitwise_or(mask[vlim1:vlim2, hlim1:hlim2,:], img)
+    img = img[vlim1:vlim2, hlim1:hlim2, :]
+    mask[vlim1:vlim2, hlim1:hlim2, :] = cv2.bitwise_or(mask[vlim1:vlim2, hlim1:hlim2, :], img)
     img = mask
-
     return img
 
 def lineProc(img_path):
